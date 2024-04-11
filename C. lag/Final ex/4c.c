3b6c008c374fa2c2a739d1232d820f0e71ef8f4f
JigarParmar19 <153288821+JigarParmar19@users.noreply.github.com>
2024-04-11 20:51:27 +0530

    Add files via upload

---
+++ C. lag/Final ex/4c.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	
+    int a,b;
+	
+	printf("enter the number:");
+	scanf("%d",a);
+	
+		b=a*a*a;
+	
+	printf("cube: %d",b);
+
+}
---
+++ C. lag/Final ex/7c.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+main()
+{
+	
+	int i,j,n;
+	
+	for(i=11;i<15;i++)
+	{
+		
+		for(j=i;j<5;j++)
+		printf("%d",j);
+		n++;
+		
+	}
+
+     printf("\n");	
+}
+
